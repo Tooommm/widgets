df722ec44baae54fcbc36e0e22fb50dccb599301
Thomas Brouillet <thomasbrouillet@gmail.com>
2020-10-30 09:51:49 +0100

    creation en cours

---
+++ src/components/Search.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from 'react';
+
+const Search = () => {
+  const [term, setTerm] = useState('');
+  console.log('EVERY RENDER')
+  useEffect(() => {
+    console.log('EVERY TIMES');
+  }, []);
+
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={e => setTerm(e.target.value)}
+            className="input"
+            />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
