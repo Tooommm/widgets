155516a5d91db56fce07bdc2ca69cc836d3a6933
Thomas Brouillet <thomasbrouillet@gmail.com>
2020-11-16 19:18:46 +0100

    finished the compononent Dropdown and Translate. TODO: Navigation from scratch

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const Accordion = ({ items }) => {
   const [activeIndex, setActiveIndex] = useState(null);
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,27 +2,36 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
   useEffect(() => {
-    const search = async () => {
-          const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
-            params: {
-              action: 'query',
-              list: 'search',
-              origin: '*',
-              format: 'json',
-              srsearch: term
-            },
-          });
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
 
-          setResults(data.query.search);
-        };
-        if (term) {
-          search();
-        }
+    return () => {
+      clearTimeout(timerId);
+    }
   }, [term]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: debouncedTerm
+        },
+      });
+
+      setResults(data.query.search);
+    };
+    search();
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
     return (
