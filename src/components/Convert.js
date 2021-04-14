510d2170b60839d450c85280f1208019572e8e75
Thomas Brouillet <thomasbrouillet@gmail.com>
2021-04-14 11:04:13 +0200

    netlify config

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,8 +1,9 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
 
-const Convert = ({language, text}) => {
-  const [translated, setTranslated] = useState('');
+import axios from "axios";
+
+const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState("");
   const [debouncedText, setDebouncedText] = useState(text);
 
   useEffect(() => {
@@ -12,25 +13,24 @@ const Convert = ({language, text}) => {
 
     return () => {
       clearTimeout(timerId);
-    }
-
-  }, [text])
+    };
+  }, [text]);
 
   useEffect(() => {
     const doTranslation = async () => {
       const { data } = await axios.post(
-        'https://translation.googleapis.com/language/translate/v2',
+        "https://translation.googleapis.com/language/translate/v2",
         {},
         {
           params: {
-          q: debouncedText,
-          target: language.value,
-          key: process.env.REACT_APP_GOOGLE_TRANSLATE_API_KEY
+            q: debouncedText,
+            target: language.value,
+            key: process.env.REACT_APP_GOOGLE_TRANSLATE_API_KEY,
           },
         }
       );
       setTranslated(data.data.translations[0].translatedText);
-    }
+    };
 
     doTranslation();
   }, [language, debouncedText]);
